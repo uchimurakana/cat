150d28b770c29dd8038d733e057072fede5e8876
kana uchimura <xxxxxx@gmail.com>
2020-01-05 11:52:20 +0900

    終わり

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -7,9 +7,27 @@ class CatsController < ApplicationController
   def create
     Neko.create!(cat_params)
   end
-  def show
+  def edit
+    @cat = Neko.find(params[:id])
   end
+  def show
+    @cat = Neko.find(params[:id])
+  end
+  def update
+    @cat = Neko.find(params[:id])
+  end
+
+  def destroy 
+    cat = Neko.find(params[:id])
+    cat.destroy
+    #destroyメソッドを使用し対象のツイートを削除する。
+    redirect_to root_path
+  end
+  
   private
+  def selling_to_show
+    item = Neko.find_by(id: params[:id])
+  end
   def cat_params
     params.permit(:name, :sex,:image,:kind,:comment )
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   resources :admins,only:[:index] do
 end
-  resources :cats ,only:[:index, :show,:edit, :new ,:create] do
+  resources :cats ,only:[:index, :show,:edit, :new ,:create,:update,:destroy] do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
